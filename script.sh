ff4a9de94a4c64bafae9126b3c3f798faa1d77bd
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-02-08 20:03:52 +0530

    shell

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you ?"
+echo "Suresh: Hello Ramesh, im fine. What are you doing?"
+echo "Ramesh: im learning devops course"
+echo "Suresh: that is great to hear."

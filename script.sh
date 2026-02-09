dde15af27ea35dee7465b02f1a84bc029007e4eb
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-02-09 20:59:35 +0530

    linux

--- script.sh
+++ script.sh
@@ -7,3 +7,12 @@ then
     echo "Run the scrip with root priveleges"
     exit 1
 fi 
+
+dnf install nginx -y
+
+if [$? -ne 0 ]
+then
+    echo "nginx installtion is failed"
+else
+    echo "successfully installed the nginx"
+fi

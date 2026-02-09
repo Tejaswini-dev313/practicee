35ed0898f32b55da8e8c6196ab3d140b07141bfd
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-02-09 19:33:16 +0530

    linux

--- script.sh
+++ script.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-FRUIT=("APPLE", "KIWI", "ORANGE")
-
-echo "Entered first fruit is ${FRUIT[0]}"
-echo "Entered second fruit is ${FRUIT[1]}"
-echo "Entered third fruit is ${FRUIT[2]}"
-
-echo "Total number of fruits are ${FRUIT[@]}"
+USER=$(id)
+DATE=${date %Y%M%D}

1191afac35e50138a0404886b907e4ecba8e5df3
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-02-11 21:19:39 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
-
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]
-then
-    echo "Run the script with root preveleges"
-    exit 1
-fi
-
-VALIDATE() {
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is.. $R Failed $N"
-        exit 1
-    else
-        echo -e "$2 is.. $G Success $N"
-    fi
-}
-
-dnf list installed nginx 
-
-if [ $? -ne 0 ]
-then 
-    echo "nginx is not installed...installing the nginx"
-    dnf install nginx -y
-    VALIDATE $? "installing nginx"
-else
-    echo "nginx already installed"
-fi
-
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed and installing."
-    dnf install mysql -y
-    VALIDATE $? "installing mysql"
-else
-    echo "mysql already installed and nothing to do"
-fi
-
-
+for i in {1..20}
+do 
+    $i
+done

135cb73913a86bc9ffa10e43303c91ff9d4f1b3e
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-02-12 19:25:49 +0530

    script

--- script.sh
+++ script.sh
@@ -1,51 +1,22 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGS_DATE=$(date)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$LOGS_DATE.log"
-mkdir -p $LOGS_FOLDER
+set -e
 
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]
-then
-    echo "Run the script with root privileges" | tee -a $LOG_FILE
-    exit 1
-fi
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
-        exit 1
-    else
-        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
-    fi
+failure(){
+    echo "Failed at : $1:$2"
 }
 
-# USAGE(){
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
 
-#     echo -e "$R USAGE:: $N sudo sh script.sh Package1 Package2..."
-#     exit 1
-# }
+echo "Hello world success"
+echooo "Hello world success"
+echo "Hello world after failure"
 
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-for Package in $@
-do 
-    dnf list installed $Package
-    if [ $? -ne 0 ]
-    then 
-        echo "Package is not installed. installing the $Package" | tee -a $LOG_FILE
-        dnf install $Package -y
-        VALIDATE $? "Installing $Package"
-    else
-        echo "$Package is already installed. nothing to do.."   | tee -a $LOG_FILE
-    fi
-done
 
+
+# trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
+
+# echo "Hello world success"
+# echooo "Hello world success"
+# echo "Hello world after failure"

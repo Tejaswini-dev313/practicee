a9cbf5f89d5ad776876115d51595c1a51fd5eecd
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-18 20:25:03 +0530

    shell

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 ARRAY=("Apple", "kiwi", "orange")
 
 echo "First fruit is ${ARRAY[0]}"
-echo "second fruit is ${ARRAY[1]}"
+echo "second fruit is $ARRAY[1]"
 echo "third fruit is ${ARRAY[2]}"
 
 echo "Total fruit is ${ARRAY[@]}"

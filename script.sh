e771ff2df5cd1e92dc261591adfaac0b6290828a
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-18 15:01:14 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Hari: Hii Teja, How are you ?"
-echo "Teja: Hello Hari. im good how are you ?"
-echo "Hari: im also fine. where are you going ?"
-echo "Teja: im going to office"
+Person1=Hari
+Person2=Teja
+
+echo "$Person1: Hii $Person2, How are you ?"
+echo "$Person2: Hello $Person1. im good how are you ?"
+echo "$Person1: im also fine. where are you going ?"
+echo "$Person2: im going to office"

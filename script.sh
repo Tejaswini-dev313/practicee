a11312a510df6a76d02f1c1ce2c4a60f9151d21e
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-25 20:14:26 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 Person1=$1
-Person2=$2
 
-SUM=$(($Person1+$Person2))
-
-echo "total number of $Person1 and $Person2 is equal to $SUM"
+if [ $Person1 -gt 20 ]
+then
+    echo "Given number is greater than 20"
+else
+    echo "Given number is less than 20"
+fi

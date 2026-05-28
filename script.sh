9625f163ef831a844106a9ded3e6eda678b2ab98
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-28 20:34:59 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    
+    if [ $USERID -ne 0 ]
+    then 
+        echo "run the script with root privileges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is success"
+    else
+        echo "$2 is...failed"
+    fi
+}
+
+CHECK_ROOT
+
 dnf list installed mysql
 
-if [ $? -ne 0 ]
+if [ $1 -ne 0 ]
 then
-    echo "mysql is not installed. please check.."
-    dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "mysql has been installed successfully"
-    else
-        echo "mysql not installed. please check"
-    fi
+    echo "mysql is not installed...please check"
+    exit 1
+    VALIDATE $? installing git
+fi
+
+dnf list installed nginx
+
+if [ $1 -ne 0 ]
+then
+    echo "nginx is not installed...please check"
+    exit 1
+    VALIDATE $? installing nginx
 fi

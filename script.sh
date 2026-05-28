660056b9f0a5e4b0cc0137d69298af1e63472ea4
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-28 20:57:25 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,49 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-CHECK_ROOT(){
-    
-    if [ $USERID -ne 0 ]
-    then 
-        echo "run the script with root privileges"
-        exit 1
-    fi
-}
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N"
-    else
-        echo -e "$2 is $G success $N"
-    fi
-}
-
-CHECK_ROOT
-
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed and going to install it"
-    dnf install mysql -y
-    VALIDATE $? "installing mysql"
-else
-    echo "mysql is already installed."
-fi
-
-dnf list installed nginx
-
-if [ $? -ne 0 ]
-then
-    echo "nginx is not installed and going to install it"
-    dnf install nginxx -y
-    VALIDATE $? "installing nginx"
-else
-    echo "nginx is already installed"
-fi
+for i in 0..20
+do
+    echo $i
+done

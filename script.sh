0358cedec03be0555bee79ffcc7ede17f6a2192c
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-05-28 21:10:35 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-for i in {0..20}
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root privileges"
+        exit 1
+    fi
+}  
+
+CHECK_ROOT
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N"
+    else
+        echo -e "$2 is $G success $N"
+    fi
+}
+
+for Package in $@
 do
-    echo $i
+    dnf list installed $@
+    
+    if [ $? -ne 0 ]
+    then 
+        echo "$Package is not installed..please check"
+        dnf install $Package -y
+        VALIDATE $? "installing $Package"
+    else
+        echo "$Package is already installed"
+    fi
 done

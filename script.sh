1b2bd59c96ac7e8cf4dc1241d6d8073b02f3f3c1
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-06-11 19:41:58 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
+person1=raju
+person2=teja
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run the script with root privileges"
-        exit 1
-    fi
-}  
-
-CHECK_ROOT
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N"
-    else
-        echo -e "$2 is $G success $N"
-    fi
-}
-
-for Package in $@
-do
-    dnf list installed $@
-    
-    if [ $? -ne 0 ]
-    then 
-        echo "$Package is not installed..please check"
-        dnf install $Package -y
-        VALIDATE $? "installing $Package"
-    else
-        echo "$Package is already installed"
-    fi
-done
+echo "$person1: Hii $person2"
+echo "$person2: hii, how are you $person1"

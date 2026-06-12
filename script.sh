e1773033f83b1fca1c6cc20c616209b6204bb656
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-06-12 19:43:50 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,59 +1,71 @@
 #!/bin/bash
 
-LOG_FOLDER="/var/log/shell-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date)
-LOGS="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
+set -e
 
-userid=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-B="\e[34m"
-N="\e[0m"
-
-CHECK_ROOT(){
-    if [ $userid -ne 0 ]
-    then 
-        echo "run the script with root priveleges" | tee -a $LOGS 
-        exit 1
-    fi
+failure(){
+    echo "failed at $1:$2"
 }
 
-CHECK_ROOT
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N" | tee -a $LOGS
-    else
-        echo -e "$2 is $G success $N" | tee -a $LOGS
-    fi
-}
+echo "hello world success"
+echooo "hello world failure"
+echo "hello world after failure"
 
-USAGE(){
-    echo "USAGE:: sudo sh script.sh package1 package2 ..."
-    exit 1
-}
+# LOG_FOLDER="/var/log/shell-logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# TIMESTAMP=$(date)
+# LOGS="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+# mkdir -p $LOG_FOLDER
 
-if [ $# -eq 0 ]
-then
-    USAGE
-fi
+# userid=$(id -u)
 
-for Package in $@
-do
+# R="\e[31m"
+# G="\e[32m"
+# B="\e[34m"
+# N="\e[0m"
 
-dnf list installed $Package | tee -a $LOGS
+# CHECK_ROOT(){
+#     if [ $userid -ne 0 ]
+#     then 
+#         echo "run the script with root priveleges" | tee -a $LOGS 
+#         exit 1
+#     fi
+# }
 
-if [ $? -ne 0 ]
-then
-    echo "$Package is not installed and going to install it" &>> $LOGS
-    dnf install $Package -y  | tee -a $LOGS
-    VALIDATE $? "$Package is installing" | tee -a $LOGS
-else
-    echo "$Package is already installed" | tee -a $LOGS
-fi
+# CHECK_ROOT
 
-done
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is $R failed $N" | tee -a $LOGS
+#     else
+#         echo -e "$2 is $G success $N" | tee -a $LOGS
+#     fi
+# }
+
+# USAGE(){
+#     echo "USAGE:: sudo sh script.sh package1 package2 ..."
+#     exit 1
+# }
+
+# if [ $# -eq 0 ]
+# then
+#     USAGE
+# fi
+
+# for Package in $@
+# do
+
+# dnf list installed $Package | tee -a $LOGS
+
+# if [ $? -ne 0 ]
+# then
+#     echo "$Package is not installed and going to install it" &>> $LOGS
+#     dnf install $Package -y  | tee -a $LOGS
+#     VALIDATE $? "$Package is installing" | tee -a $LOGS
+# else
+#     echo "$Package is already installed" | tee -a $LOGS
+# fi
+
+# done

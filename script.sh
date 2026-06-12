6307472c8a857089ee874cf88fe76bd6227ec2b8
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-06-12 12:43:37 +0530

    shell

--- script.sh
+++ script.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
-userid=$(id -u)
+# userid=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-B="\e[34m"
-N="\e[0m"
+# R="\e[31m"
+# G="\e[32m"
+# B="\e[34m"
+# N="\e[0m"
 
-CHECK_ROOT(){
-    if [ $userid -ne 0 ]
-    then 
-        echo "run the script with root priveleges"
-        exit 1
-    fi
-}
+# CHECK_ROOT(){
+#     if [ $userid -ne 0 ]
+#     then 
+#         echo "run the script with root priveleges"
+#         exit 1
+#     fi
+# }
 
-CHECK_ROOT
+# CHECK_ROOT
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N"
-    else
-        echo -e "$2 is $G success $N"
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is $R failed $N"
+#     else
+#         echo -e "$2 is $G success $N"
+#     fi
+# }
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-    echo "mysql is not installed and going to install it"
-    dnf install mysql -y
-    VALIDATE $? "installing mysql"
-else
-    echo -e "$B mysql is already installed $N"
-fi
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not installed and going to install it"
+#     dnf install mysql -y
+#     VALIDATE $? "installing mysql"
+# else
+#     echo -e "$B mysql is already installed $N"
+# fi
 
-dnf list installed git
+# dnf list installed git
 
-if [ $? -ne 0 ]
-then 
-    echo "git is not installed and going to install it"
-    dnf install git -y
-    VALIDATE $? "insttalling git"
-else
-    echo "git already installed"
-fi
+# if [ $? -ne 0 ]
+# then 
+#     echo "git is not installed and going to install it"
+#     dnf install git -y
+#     VALIDATE $? "insttalling git"
+# else
+#     echo "git already installed"
+# fi
  
-
-
+for i in {0..25}
+do
+    echo $i
+done
 

352a4378dbcc8afff9372147f601a172352d9c92
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2026-06-12 12:28:12 +0530

    shell

--- script.sh
+++ script.sh
@@ -6,21 +6,15 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-if [ $userid -ne 0 ]
-then 
- echo "run the script with root priveleges"
- exit 1
-fi
+CHECK_ROOT(){
+    if [ $userid -ne 0 ]
+    then 
+        echo "run the script with root priveleges"
+        exit 1
+    fi
+}
 
-dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-    echo "mysql is not installed..going to install it"
-    dnf install mysql -y
-else
-    echo -e "$R mysql is alredy installed $N"
-fi 
 
 
 

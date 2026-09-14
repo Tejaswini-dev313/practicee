f956ccfa3130e8067e11873b57e962d127724df3
chandrahas <chandrahasreddy644@gmail.com>
2026-09-14 19:59:42 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
+# person1=$1
+# person2=$2
 
-echo "Hii, ${person1}. how are you ?"
-echo "Hello ${person2}, im fine. How are you?"
+# echo "Hii, ${person1}. how are you ?"
+# echo "Hello ${person2}, im fine. How are you?"
+
+echo "Please enter USERNAME"
+
+read -s USERNAME
+
+echo "Entered username is: $USERNAME"
+
+echo "Please enter the PASSWORD"
+
+read PASSWORD
+
+echo "Entered password is: $PASSWORD"

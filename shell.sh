977c3a085d4f1d180b5fe188e767a64c4e8b68c5
chandrahas <chandrahasreddy644@gmail.com>
2026-09-15 19:33:27 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,9 +1,3 @@
 #!/bin/bash/
 
-Fruit=("Apple" "Kiwi" "Orange")
-
-echo "First fruit is ${Fruit[0]}"
-echo "Second fruit is ${Fruit[1]}"
-echo "Third fruit is ${Fruit[2]}"
-
-echo "Total number of fruits are ${Fruit[@]}"
+User=$(id -u)

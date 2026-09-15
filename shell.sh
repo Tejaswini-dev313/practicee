fd95ee0a72ca0dd4c71e83940488c3eddea43bb7
chandrahas <chandrahasreddy644@gmail.com>
2026-09-15 19:31:38 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash/
 
-Fruit=("Apple", "Kiwi", "Orange")
+Fruit=("Apple" "Kiwi" "Orange")
 
 echo "First fruit is ${Fruit[0]}"
 echo "Second fruit is ${Fruit[1]}"
 echo "Third fruit is ${Fruit[2]}"
 
-echo "Total number of fruits are ${@}"
+echo "Total number of fruits are $@"

d41e693f73d2c4418a97828029be20796930c9fb
chandrahas <chandrahasreddy644@gmail.com>
2026-09-19 20:00:52 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,41 +1,46 @@
 #!/bin/bash/
 
-userid=$(id -u)
+# userid=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
 
-check_root(){
+# check_root(){
 
-if [ $userid -ne 0 ]
-then 
-    echo "run the script with root previliges"
-    exit 1
-fi
-}
+# if [ $userid -ne 0 ]
+# then 
+#     echo "run the script with root previliges"
+#     exit 1
+# fi
+# }
 
-check_root
+# check_root
 
-validate(){
+# validate(){
 
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N"
-        exit 1
-    else
-        echo -e "$2 is $G success $N"
-    fi
-}
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is $R failed $N"
+#         exit 1
+#     else
+#         echo -e "$2 is $G success $N"
+#     fi
+# }
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed. install mysql"
-    dnf install mysql -y
-    validate $? "installing mysql" 
-else
-    echo -e "$Y mysql is already installed $N"
-fi
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed. install mysql"
+#     dnf install mysql -y
+#     validate $? "installing mysql" 
+# else
+#     echo -e "$Y mysql is already installed $N"
+# fi
+
+for i in {0..20}
+do
+    echo "$i"
+done

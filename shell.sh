b08ff1a02a79608e72aae9ef1ad9ec212d6c664f
chandrahas <chandrahasreddy644@gmail.com>
2026-09-19 19:30:49 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -2,11 +2,26 @@
 
 userid=$(id -u)
 
+check_root(){
+
 if [ $userid -ne 0 ]
 then 
     echo "run the script with root previliges"
     exit 1
 fi
+}
+
+check_root
+
+if [ $1 -ne 0 ]
+then
+    echo "$2 is failed"
+    exit 1
+else
+    echo "$2 is success"
+fi
+
+validate(){
 
 dnf list installed mysql
 
@@ -14,6 +29,8 @@ if [ $? -ne 0 ]
 then
     echo "mysql is not installed. install mysql"
     dnf install mysql -y
+    validate $? installation 
 else
     echo "mysql is already installed"
 fi
+} 

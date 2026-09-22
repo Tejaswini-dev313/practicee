e5d91582e20eb59da9fb52a6bec10127574c6c87
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 22:06:28 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -7,8 +7,7 @@ while IFS= read -r line
 do 
     usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     partiation=$(echo $line | awk -F " " '{print $NF}')
-
-    if [ $disk_usage -ge $disk_threshold ]
+    if [ $usage -ge $disk_threshold ]
     then
         echo "$disk_usage is reached the threshold value. please check"
     fi

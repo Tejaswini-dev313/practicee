907688d62a02a5f0a619ef016e0c7c4eb697c3a0
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 21:58:57 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@
 disk_usage=$(df -hT | grep xfs)
 disk_threshold=1
 
-while IFS read -r line
+while IFS= read -r line
 do 
     usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     partiation=$(echo $line | awk -F " " '{print $NF}')

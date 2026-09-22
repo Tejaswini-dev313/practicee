8adadd4d9ad73be177f8782d4dce2d470ec748b7
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 21:11:46 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -8,15 +8,27 @@ then
     exit 1
 fi
 
-for package in "$@"
+for package in $@ # $@ refers to all arguments passed to it
 do
     dnf list installed $package
-
     if [ $? -ne 0 ]
     then
-        echo "package is not installed. install $package"
+        echo "$package is not installed, going to install it"
         dnf install $package -y
+        VALIDATE $? "installing $package"
     else
-        echo "$package package is already installed. nothing to do"
+        echo "$package is already installed."
     fi
 done
+
+# for package in $@
+# do
+#     dnf list installed $package
+#     if [ $? -ne 0 ]
+#     then
+#         echo "package is not installed. install $package"
+#         dnf install $package -y
+#     else
+#         echo "$package package is already installed. nothing to do"
+#     fi
+# done

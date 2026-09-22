819cbf8be7309f50cd0b781a9be06361615b5e85
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 19:24:40 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,54 +1,16 @@
-#!/bin/bash/
-
-set -o pipefail
-
-path="/var/log/shell_script"
-timestamp=$(date +%Y-%m-%d-%H-%M-%S)
-script_name=$(echo $0 | cut -d "." -f1)
-Log_file="$path/$script_name-$timestamp.log"
-
-mkdir -p $path
-
-userid=$(id -u)
+#!/bin/bash
 
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
-check_root(){
-
-if [ $userid -ne 0 ]
-then 
-    echo "run the script with root previliges" | tee -a $Log_file
-    exit 1
-fi
+failure(){
+    echo "failed at $1:$2"
+    
 }
 
-check_root
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-validate(){
-
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N" | tee -a $Log_file
-        exit 1
-    else
-        echo -e "$2 is $G success $N" | tee -a $Log_file
-    fi
-}
-
-for package in "$@"
-do
-
-dnf list installed $package | tee -a $Log_file
-
-if [ $? -ne 0 ]
-then
-    echo "$package is not installed. installing $package"
-    dnf install $package -y | tee -a $Log_file
-    validate $? "installing $package" 
-else
-    echo -e "$Y $package is already installed $N" | tee -a $Log_file
-fi
-done
+echo "Hello world 1"
+echo "hello000 world 2"
+echo "Hello world 3"

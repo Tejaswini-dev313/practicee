9a66080ecbd6b944041b7506836f3e3b7473d5ce
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 21:58:05 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 
-user=$(id -u)
+disk_usage=$(df -hT | grep xfs)
+disk_threshold=1
 
-if [ $user -ne 0 ]
-then
-    echo "run the script with root privileges"
-    exit 1
-fi
-
-VALIDATE(){
-
-    if [ $1 -ne 0 ]
+while IFS read -r line
+do 
+    usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    partiation=$(echo $line | awk -F " " '{print $NF}')
+    if [ $disk_threshold -ge 1 ]
     then
-        echo "$2 is failed"
-    else
-        echo "$2 is success"
+        echo "$disk_threshold is reached the threshold value. please check"
     fi
-
-}
-
-for package in $@
-do
-    dnf list installed $package
-    if [ $? -ne 0 ]
-    then
-        echo "package is not installed. install $package"
-        dnf install $package -y
-        VALIDATE $? "installation"
-    else
-        echo "$package package is already installed. nothing to do"
-        VALIDATE $? "installation"
-    fi
-
-    systemctl start nginx
-    VALIDATE $? "started"
-
-done
+done <<< disk_usage

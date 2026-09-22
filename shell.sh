a6d3b7fb46783cc2c7e9ba82e760a8a26f8f6b49
chandrahas <chandrahasreddy644@gmail.com>
2026-09-22 17:51:09 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,56 +1,61 @@
 #!/bin/bash/
 
+path="/var/log/shell_script"
+timestamp=$(date)
+script_name=$(echo $0 | cut -d "." -f1)
+Log_file="$path/$timestamp-$script_name.log"
 
+mkdir -p $Log_file
 
-userid=$(id -u)
+# userid=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
 
-check_root(){
+# check_root(){
 
-if [ $userid -ne 0 ]
-then 
-    echo "run the script with root previliges"
-    exit 1
-fi
-}
+# if [ $userid -ne 0 ]
+# then 
+#     echo "run the script with root previliges"
+#     exit 1
+# fi
+# }
 
-check_root
+# check_root
 
-validate(){
+# validate(){
 
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R failed $N"
-        exit 1
-    else
-        echo -e "$2 is $G success $N"
-    fi
-}
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is $R failed $N"
+#         exit 1
+#     else
+#         echo -e "$2 is $G success $N"
+#     fi
+# }
 
-for package in "$@"
-do
+# for package in "$@"
+# do
 
-dnf list installed $package
+# dnf list installed $package
 
-if [ $? -ne 0 ]
-then
-    echo "$package is not installed. installing $package"
-    dnf install $package -y
-    validate $? "installing $package" 
-else
-    echo -e "$Y $package is already installed $N"
-fi
+# if [ $? -ne 0 ]
+# then
+#     echo "$package is not installed. installing $package"
+#     dnf install $package -y
+#     validate $? "installing $package" 
+# else
+#     echo -e "$Y $package is already installed $N"
+# fi
 
-dnf remove $package -y
+# dnf remove $package -y
 
-validate $? "removing $package"
+# validate $? "removing $package"
 
-if [ $? -ne 0 ]
-then 
-    echo "$package not deleted. check the issue"
-fi
-done
+# if [ $? -ne 0 ]
+# then 
+#     echo "$package not deleted. check the issue"
+# fi
+# done
